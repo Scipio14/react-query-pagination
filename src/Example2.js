6be991b37697db968c1dba3c1bd1ec0e57145274
Federico Macías <fma.abogado@gmail.com>
2023-01-13 20:35:20 -0600

    First commit

---
+++ src/Example2.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "react-query";
+import { getUsersPage } from "./api/axios";
+import User from "./User";
+import PageButton from "./PageButton";
+
+const Example2 = () => {
+  const [page, setPage] = useState(1);
+  const {
+    isLoading,
+    isError,
+    error,
+    data: users,
+    isFetching,
+    isPreviousData,
+  } = useQuery(["/users", page], () => getUsersPage(page), {
+    keepPreviousData: true,
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error: {error.message}</p>;
+
+  const content = users.data.map((user) => <User key={user.id} user={user} />);
+
+  // const nextPage = () => setPage((prev) => prev + 1);
+  const lastPage = () => setPage(users.total_pages);
+  // const prevPage = () => setPage((prev) => prev - 1);
+  const firstPage = () => setPage(1);
+
+  const pagesArray = Array(users.total_pages).fill().map((_,index)=> index + 1)
+
+  const nav = (
+    <nav className="nav-ex2">
+      <button
+        onClick={firstPage}
+        disabled={isPreviousData || page === 1}
+      >&lt;&lt;</button>
+      {pagesArray.map(pg=><PageButton key={pg} pg={pg} setPage={setPage} isPreviousData={isPreviousData}/>)}
+      <button
+        onClick={lastPage}
+        disabled={isPreviousData || page === users.total_pages}
+      >&gt;&gt;</button>
+    </nav>
+  )
+
+  return(
+    <>
+      {nav}
+      {isFetching && <span className="loading">Loading...</span>}
+      {content}
+    </>
+  )
+};
+export default Example2;
